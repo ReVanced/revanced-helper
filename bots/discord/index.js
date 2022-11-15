7f9ca77e0331ec143160ee51ed7c3aa9e4e70b9c
GramingFoxTeam <reis@turkeylist.gq>
2022-11-15 21:24:05 +0300

    feat: initialize helper client

--- bots/discord/index.js
+++ bots/discord/index.js
@@ -1,8 +1,13 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { readFileSync } from 'node:fs';
+import HelperClient from '../../client/index.js';
 
 const configJSON = readFileSync('../config.json', 'utf-8');
 const config = JSON.parse(configJSON);
+
+const helper = new HelperClient(config.server);
+
+helper.connect();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
@@ -11,6 +16,16 @@ client.on(Events.MessageCreate, async (msg) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
+
+});
+
+// The ReVanced Helper events.
+
+helper.on('aiResponse', async (aiRes) => {
+
+});
+
+helper.on('ocrResponse', async (aiRes) => {
 
 });
 
---
+++ client/index.js
@@ -0,0 +1,78 @@
+import { createConnection } from 'node:net';
+import { serialize, deserialize } from 'bson';
+import EventEmitter from 'node:events';
+
+class HelperClient extends EventEmitter {
+    constructor({ server }) {
+        super();
+        if (!server.port) throw new Error('You did not specify the server port.');
+        this.server = server;
+    };
+
+    connect() {
+        this.client = createConnection(this.server.port, this.server.host ? this.server.host : 'localhost', () => {
+            this.emit('connect');
+        });
+
+        this.client.on('data', (data) => {
+            const eventData = deserialize(data);
+
+            switch (eventData.op) {
+                case 2: {
+                    // The 'aiResponse' event.
+
+                    this.emit('aiResponse', eventData);
+                };
+
+                case 6: {
+                    // The 'ocrResponse' event.
+
+                    this.emit('ocrResponse', eventData);
+                };
+            };
+        });
+    };
+
+    sendData(data) {
+        this.client.write(serialize(data));
+        return;
+    };
+
+    scanText(text, id) {
+        this.sendData({
+            op: 1,
+            id, 
+            text
+        });
+
+        return;
+    };
+
+    scanImage(url, id) {
+        this.sendData({
+            op: 5,
+            id, 
+            url
+        });
+
+        return;
+    };
+
+    sendTrainData(text, label) {
+        this.sendData({
+            op: 5,
+            label, 
+            text
+        });
+
+        return;
+    };
+
+    trainAI() {
+        this.sendData({ op: 4 });
+
+        return;
+    };
+};
+
+export default HelperClient;
