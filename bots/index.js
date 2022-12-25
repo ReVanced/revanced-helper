84a290933ff63d3a89509cd7c808718069e32a32
GramingFoxTeam <reis@turkeylist.gq>
2022-12-25 16:07:13 +0300

    feat: run only one bot when bot name given

--- bots/index.js
+++ bots/index.js
@@ -4,8 +4,10 @@ const botFolders = readdirSync('./', { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name);
 
+const runBotOnly = process.argv[2] ? process.argv[2] : null;
 for (const botFolder of botFolders) {
     if (botFolder === 'node_modules') continue;
+    if (runBotOnly && runBotOnly !== botFolder) continue;
     const botIndex = await import(`./${botFolder}/index.js`);
     botIndex.default();
 }
