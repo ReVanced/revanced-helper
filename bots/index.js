ca52b15280c2870d0f211017589a8799ee5ecda9
GramingFoxTeam <reis@turkeylist.gq>
2022-12-25 15:52:51 +0300

    fix: node_modules being treated as a bot folder

--- bots/index.js
+++ bots/index.js
@@ -5,6 +5,7 @@ const botFolders = readdirSync('./', { withFileTypes: true })
     .map(dirent => dirent.name);
 
 for (const botFolder of botFolders) {
+    if (botFolder === 'node_modules') continue;
     const botIndex = await import(`./${botFolder}/index.js`);
     botIndex.default();
 }
