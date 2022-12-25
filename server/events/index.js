54ec6232eed97031fd8026b8756d2d5c51d0f20f
GramingFoxTeam <reis@turkeylist.gq>
2022-12-25 13:46:44 +0300

    fix: trainData and train no longer being an event

--- server/events/index.js
+++ server/events/index.js
@@ -1,6 +1,4 @@
 import runAI from './ai.js';
-import trainAI from './trainAI.js';
 import runOCR from './ocr.js';
-import addTrainData from './addTrainData.js';
 
-export { runAI, trainAI, runOCR, addTrainData };
+export { runAI, runOCR };
