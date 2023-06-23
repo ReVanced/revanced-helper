6b41339d7734ea1c4f3c43b56da0ae4af8168436
GramingFoxTeam <reis@turkeylist.gq>
2023-06-23 12:34:53 +0300

    fix: bot crashing when a webhook msg is sent

--- apps/bot-discord/src/events/messageCreate.js
+++ apps/bot-discord/src/events/messageCreate.js
@@ -4,7 +4,7 @@ export default {
 	name: Events.MessageCreate,
 	once: false,
 	execute(helper, config, msg) {
-		if (!msg.guild || msg.system) return;
+		if (!msg.guild || msg.system || msg.webhookId) return;
 		if (msg.member.roles.cache.some(role => role.id === config.discord.ignoreRole)) return;
 		if (msg.attachments.first() && msg.attachments.first().contentType.startsWith('image')) {
 			helper.scanImage(msg.attachments.first().url, `${msg.channelId}/${msg.id}`);
