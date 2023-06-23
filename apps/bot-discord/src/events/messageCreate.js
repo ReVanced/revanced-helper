5e29c757795822a531694517221699781b35f88c
GramingFoxTeam <reis@turkeylist.gq>
2023-06-23 11:23:27 +0300

    feat: role blacklist

--- apps/bot-discord/src/events/messageCreate.js
+++ apps/bot-discord/src/events/messageCreate.js
@@ -3,7 +3,9 @@ import { Events } from 'discord.js';
 export default {
 	name: Events.MessageCreate,
 	once: false,
-	execute(helper, _, msg) {
+	execute(helper, config, msg) {
+		if (!msg.guild) return;
+		if (msg.member.roles.cache.some(role => role.id === config.discord.ignoreRole)) return;
 		if (msg.attachments.first() && msg.attachments.first().contentType.startsWith('image')) {
 			helper.scanImage(msg.attachments.first().url, `${msg.channelId}/${msg.id}`);
 		}
