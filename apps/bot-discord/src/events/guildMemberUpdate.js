6a8e9ec8fcf0663caee3eb326fd1833a8356f0e4
GramingFoxTeam <reis@turkeylist.gq>
2023-09-04 15:58:07 +0300

    fix(bot-discord): rename parameters

--- apps/bot-discord/src/events/guildMemberUpdate.js
+++ apps/bot-discord/src/events/guildMemberUpdate.js
@@ -4,7 +4,7 @@ import cureUsername from '../utils/cureUsername.js';
 export default {
     name: Events.GuildMemberUpdate,
     once: false,
-    async execute(_, _, _, newMember) {
+    async execute(_, config, oldMember, newMember) {
         cureUsername(newMember);
     }
 };
