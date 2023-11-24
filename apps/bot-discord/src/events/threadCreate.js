921d98e0ad3684ac85f1499a523aa25a7b1a3e47
PalmDevs <palmpasuthorn@gmail.com>
2023-11-24 22:54:41 +0700

    chore(init): initialize a new project

--- apps/bot-discord/src/events/threadCreate.js
+++
@@ -1,9 +0,0 @@
-import { Events } from 'discord.js';
-
-export default {
-	name: Events.ThreadCreate,
-	once: false,
-	async execute(helper, _, thread) {
-       helper.scanText(thread.name.toLowerCase(), thread.id);
-	}
-};
--- apps/server/src/events/index.js
+++
@@ -1,5 +0,0 @@
-import runAI from './runAI.js';
-import runOCR from './runOCR.js';
-import trainAI from './trainAI.js';
-
-export { runAI, runOCR, trainAI };
