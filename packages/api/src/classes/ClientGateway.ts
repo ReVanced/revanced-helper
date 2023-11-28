b717fd135f98e1412f1a110e9dc89c1833e77149
PalmDevs <palmpasuthorn@gmail.com>
2023-11-29 00:56:03 +0700

    chore: apply biome code fixes

--- packages/api/src/classes/ClientGateway.ts
+++ packages/api/src/classes/ClientGateway.ts
@@ -44,9 +44,7 @@ export default class ClientGateway {
                     rs()
                 })
 
-                this.#socket.on('close', () =>
-                    this.#handleDisconnect(DisconnectReason.Generic),
-                )
+                this.#socket.on('close', () => this.#handleDisconnect(DisconnectReason.Generic))
 
                 this.#listen()
                 this.ready = true
@@ -102,14 +100,10 @@ export default class ClientGateway {
      * @returns A promise that resolves when the packet has been sent
      */
     send<TOp extends ClientOperation>(packet: Packet<TOp>) {
-        this.#throwIfDisconnected(
-            'Cannot send a packet when already disconnected from the server',
-        )
+        this.#throwIfDisconnected('Cannot send a packet when already disconnected from the server')
 
         return new Promise<void>((resolve, reject) =>
-            this.#socket.send(serializePacket(packet), err =>
-                err ? reject(err) : resolve(),
-            ),
+            this.#socket.send(serializePacket(packet), err => (err ? reject(err) : resolve())),
         )
     }
 
@@ -117,9 +111,7 @@ export default class ClientGateway {
      * Disconnects from the WebSocket API
      */
     disconnect() {
-        this.#throwIfDisconnected(
-            'Cannot disconnect when already disconnected from the server',
-        )
+        this.#throwIfDisconnected('Cannot disconnect when already disconnected from the server')
 
         this.#handleDisconnect(DisconnectReason.Generic)
     }
@@ -144,25 +136,17 @@ export default class ClientGateway {
             switch (packet.op) {
                 case ServerOperation.Hello: {
                     // eslint-disable-next-line no-case-declarations
-                    const data = Object.freeze(
-                        (packet as Packet<ServerOperation.Hello>).d,
-                    )
+                    const data = Object.freeze((packet as Packet<ServerOperation.Hello>).d)
                     this.config = data
                     this.#emitter.emit('hello', data)
                     this.#startHeartbeating()
                     break
                 }
                 case ServerOperation.Disconnect:
-                    return this.#handleDisconnect(
-                        (packet as Packet<ServerOperation.Disconnect>).d.reason,
-                    )
+                    return this.#handleDisconnect((packet as Packet<ServerOperation.Disconnect>).d.reason)
                 default:
                     return this.#emitter.emit(
-                        uncapitalize(
-                            ServerOperation[
-                                packet.op
-                            ] as ClientGatewayServerEventName,
-                        ),
+                        uncapitalize(ServerOperation[packet.op] as ClientGatewayServerEventName),
                         // @ts-expect-error TypeScript doesn't know that the lines above negate the type enough
                         packet,
                     )
@@ -172,8 +156,7 @@ export default class ClientGateway {
 
     #throwIfDisconnected(errorMessage: string) {
         if (this.disconnected !== false) throw new Error(errorMessage)
-        if (this.#socket.readyState !== this.#socket.OPEN)
-            throw new Error(errorMessage)
+        if (this.#socket.readyState !== this.#socket.OPEN) throw new Error(errorMessage)
     }
 
     #handleDisconnect(reason: DisconnectReason) {
@@ -222,14 +205,10 @@ export type ClientGatewayEventHandlers = {
         packet: Packet<typeof ServerOperation[Capitalize<K>]>,
     ) => Promise<void> | void
 } & {
-    hello: (
-        config: NonNullable<ClientGateway['config']>,
-    ) => Promise<void> | void
+    hello: (config: NonNullable<ClientGateway['config']>) => Promise<void> | void
     ready: () => Promise<void> | void
     packet: (packet: Packet<ServerOperation>) => Promise<void> | void
     disconnect: (reason: DisconnectReason) => Promise<void> | void
 }
 
-export type ReadiedClientGateway = RequiredProperty<
-    InstanceType<typeof ClientGateway>
->
+export type ReadiedClientGateway = RequiredProperty<InstanceType<typeof ClientGateway>>
