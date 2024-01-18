a3d8d1c9b79a3288aac7218b2d2d7356151c9f78
Palm <PalmDevelops@gmail.com>
2024-01-18 22:43:19 +0700

    fix(packages/api)!: handle dead connections better

--- packages/shared/src/constants/HumanizedDisconnectReason.ts
+++ packages/shared/src/constants/HumanizedDisconnectReason.ts
@@ -9,6 +9,7 @@ const HumanizedDisconnectReason = {
     [DisconnectReason.TimedOut]: 'has timed out',
     [DisconnectReason.ServerError]: 'has been disconnected due to an internal server error',
     [DisconnectReason.NeverConnected]: 'had never connected to the server',
+    [DisconnectReason.PlannedDisconnect]: 'has disconnected on its own',
 } as const satisfies Record<DisconnectReason, string>
 
 export default HumanizedDisconnectReason
