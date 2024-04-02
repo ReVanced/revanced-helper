d830e48bc2de7aa457eab3a5f96ae652a93178f9
PalmDevs <git@palmdevs.me>
2024-04-02 19:29:29 +0700

    fix(bots/discord): remove useless feature

--- bots/discord/src/utils/discord/security.ts
+++
@@ -1,7 +0,0 @@
-import type { Guild } from 'discord.js'
-import { logger } from '../../context'
-
-export const leaveDisallowedGuild = (guild: Guild) => {
-    logger.warn(`Server ${guild.name} (${guild.id}) is not allowed to use this bot.`)
-    return guild.leave().then(() => logger.debug(`Left guild ${guild.name} (${guild.id})`))
-}
