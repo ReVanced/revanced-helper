fa0159c3a8dd4dad8778ccdb75b9e7c02ebbb64f
PalmDevs <git@palmdevs.me>
2024-06-23 14:01:06 +0700

    fix(bots/discord): wrong command file path being imported

--- bots/discord/src/utils/fs.ts
+++ bots/discord/src/utils/fs.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'bun'
 export const listAllFilesRecursive = (dir: string): string[] =>
     readdirSync(dir, { recursive: true, withFileTypes: true })
         .filter(x => x.isFile())
-        .map(x => join(dir, x.name))
+        .map(x => join(x.parentPath, x.name))
 
 export const pathJoinCurrentDir = (importMetaUrl: string, ...objects: [string, ...string[]]) =>
     join(dirname(fileURLToPath(importMetaUrl)), ...objects)
