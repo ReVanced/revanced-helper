c53a89ff99f36dd165f2957578ef4ab283869815
PalmDevs <git@palmdevs.me>
2024-06-24 01:14:15 +0700

    chore(bots/discord): setup drizzle kit

---
+++ bots/discord/drizzle.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+    dialect: 'sqlite',
+    schema: './src/database/schemas.ts',
+    dbCredentials: {
+        url: 'file:./db.sqlite',
+    },
+})
