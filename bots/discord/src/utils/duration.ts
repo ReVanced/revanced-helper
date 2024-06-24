83c314ef5f721abc355272db0e4c182dcfe5d943
PalmDevs <git@palmdevs.me>
2024-06-24 20:47:14 +0700

    fix(bots/discord/utils/duration): fix empty string returning with duration is 0

--- bots/discord/src/utils/duration.ts
+++ bots/discord/src/utils/duration.ts
@@ -12,6 +12,8 @@ export const parseDuration = (duration: string) => {
 }
 
 export const durationToString = (duration: number) => {
+    if (duration === 0) return '0s'
+
     const days = Math.floor(duration / (24 * 60 * 60 * 1000))
     const hours = Math.floor((duration % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000))
     const minutes = Math.floor((duration % (60 * 60 * 1000)) / (60 * 1000))
