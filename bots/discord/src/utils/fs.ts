09dc70632da0597fdb26677acee3f6fccbb2b9b5
PalmDevs <git@palmdevs.me>
2024-07-09 01:07:27 +0700

    fix(bots/discord/scripts): unintentional escaping on windows

--- bots/discord/src/utils/fs.ts
+++ bots/discord/src/utils/fs.ts
@@ -1,10 +1,11 @@
 import { readdirSync, writeFileSync } from 'fs'
-import { join, relative } from 'path'
+import { join, sep as pathSep, relative } from 'path'
+import { sep as posixPathSep } from 'path/posix'
 
 export const listAllFilesRecursive = (dir: string): string[] =>
     readdirSync(dir, { recursive: true, withFileTypes: true })
         .filter(x => x.isFile())
-        .map(x => join(x.parentPath, x.name))
+        .map(x => join(x.parentPath, x.name).replaceAll(pathSep, posixPathSep))
 
 export const generateCommandsIndex = (dirPath: string) => generateIndexes(dirPath, x => !x.endsWith('types.ts'))
 
@@ -13,7 +14,7 @@ export const generateEventsIndex = (dirPath: string) => generateIndexes(dirPath)
 const generateIndexes = async (dirPath: string, pathFilter?: (path: string) => boolean) => {
     const files = listAllFilesRecursive(dirPath)
         .filter(x => (x.endsWith('.ts') && !x.endsWith('index.ts') && pathFilter ? pathFilter(x) : true))
-        .map(x => relative(dirPath, x))
+        .map(x => relative(dirPath, x).replaceAll(pathSep, posixPathSep))
 
     writeFileSync(
         join(dirPath, 'index.ts'),
